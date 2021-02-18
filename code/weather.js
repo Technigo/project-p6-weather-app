d533c625e1e3df72ee41ac84d700c7d296164c64
Christoffer Torninger <torninger@gmail.com>
2021-02-18 17:24:42 +0100

    Initial commit
    Making sure it's all getting up to GitHub.

--- code/weather.js
+++ code/weather.js
@@ -1,13 +1,14 @@
 // All the DOM selectors stored as short variables
-const shortDescription = document.getElementById("shortDescription")
+
 const body = document.getElementById("body")
-const temperature = document.getElementById("temperature")
-const sunrise = document.getElementById("sunrise")
-const sunset = document.getElementById("sunset")
-const description = document.getElementById("description")
-const forecast = document.getElementById("forecast")
 const icon = document.getElementById("icon")
+const sunset = document.getElementById("sunset")
+const sunrise = document.getElementById("sunrise")
+const forecast = document.getElementById("forecast")
+const description = document.getElementById("description")
+const temperature = document.getElementById("temperature")
+const shortDescription = document.getElementById("shortDescription")
 
 // Global variables
-const API_KEY = "12345"
-let city = "Stockholm"
+const API_KEY = "6bb7973de3d5499c2cfa5c1f2092da78"
+let city = "Belgrade"
