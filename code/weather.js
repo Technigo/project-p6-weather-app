c345b4f7929cf19a866398fda55da7d549c7d827
lodanu <75158529+lodanu@users.noreply.github.com>
2021-02-26 20:24:30 +0100

    updated headers, forcast and temperature

--- code/weather.js
+++ code/weather.js
@@ -7,31 +7,43 @@ const sunset = document.getElementById("sunset")
 const description = document.getElementById("description")
 const forecast = document.getElementById("forecast")
 const icon = document.getElementById("icon")
+const headline = document.getElementById("headline")
 
 // Global variables
 const API_KEY = "c4fbece7d9084df316c81a5f217e2d95"
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+const B_URL = "https://api.openweathermap.org/data/2.5/weather"
 let city = "Stockholm"
+const F_URL = `https://api.openweathermap.org/data/2.5/forecast`
 
-const URL = `${BASE_URL}?q=${city}&units=metric&APPID=${API_KEY}`
+const weatherURL = `${B_URL}?q=${city}&units=metric&APPID=${API_KEY}`
+const forecastURL = `${F_URL}?q=${city}&units=metric&APPID=${API_KEY}`
 
-const convertTime = time => {
-    const date = new Date (time * 1000)
-    const convertedTime = date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit"})
+headline.innerHTML = `The weather in ${city} today`
+
+function convertTime(time) {
+    const date = new Date(time * 1000)
+    const convertedTime = date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
     return convertedTime
 }
 
-fetch(URL)
-    .then(res => res.json())
+
+
+fetch(weatherURL)
+    .then(response => response.json())
     .then(data => {        
-        console.log(data)
-        shortDescription.innerHTML = data.weather[0].description 
+
+        shortDescription.innerHTML = data.weather[0].description
+        temperature.innerHTML = `${data.main.temp.toFixed()} °C` 
+        feelsLike.innerHTML = `feels like ${data.main.feels_like.toFixed()} °C`
+        sunrise.innerHTML = `Sunrise: ${convertTime(data.sys.sunrise)}`
+        sunset.innerHTML = `Sunset: ${convertTime(data.sys.sunset)}`
+        
 
         if (data.weather[0].main === "Clouds") {
             body.classList.remove(...body.classList)
             body.classList.add("cloudy")
             icon.src = "./assets/cloud.svg"
-            description.innerHTML = `It's so cloudy today in ${city}`
+            description.innerHTML = `It's cloudy today in ${city}`
         }
         else if (data.weather[0].main === "Rain") {
             body.classList.remove(...body.classList)
@@ -45,8 +57,44 @@ fetch(URL)
             icon.src = "./assets/sun.svg"
             description.innerHTML = `It's sunny today in ${city}`
         }
-
-        sunrise.innerHTML = `Sunrise: ${convertTime(data.sys.sunrise)}`
-        sunset.innerHTML = `Sunset: ${convertTime(data.sys.sunset)}`
+        else if (data.weather[0].main === "Unknown") {
+            body.classList.remove(...body.classList)
+            body.classList.add("unknown")
+            icon.src = "./assets/unkown.svg"
+            description.innerHTML = `The weather in ${city} is unkown today.`
+        }
+        else if (data.weather[0].main === "Clear") {
+            body.classList.remove(...body.classList)
+            body.classList.add("clear")
+            icon.src = "./assets/sun.svg"
+            description.innerHTML = `Beautiful clear sky today in ${city}.`
+        }
+        
     })
+ 
     
+fetch(forecastURL)
+    .then(response => response.json())
+    .then(data => {
+
+        const filteredForecast = data.list.filter(item => item.dt_txt.includes('12:00'))
+console.log(filteredForecast)
+        const convertToWeekday = (date) => {
+            const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+            const wholeDate = new Date(date)
+            const dayNumber = wholeDate.getDay()
+            const dayName = (dayNames[dayNumber])
+            return dayName
+        }
+
+        filteredForecast.forEach(day => {
+            forecast.innerHTML += `
+                <div>
+                    <p>${convertToWeekday(day.dt_txt)}</p>
+                    <p>${day.main.temp.toFixed(0)} °C</p>
+                </div>
+                `
+        })
+    })
+
+
